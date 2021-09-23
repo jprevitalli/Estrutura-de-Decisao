70510e00133a03b083b4af5ace20b6fb38253a9e
Jonathan Previtalli <jonathan.previtalli@gmail.com>
2021-09-23 11:34:23 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaDecisão/Estrutura.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstruturaDecisão
+{
+    public partial class TelaEstrutura : Form
+    {
+        public TelaEstrutura()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDescobrir_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32 (nNum01.Value);
+            int n2 = Convert.ToInt32(nNum02.Value);
+            int n3 = Convert.ToInt32(nNum03.Value);
+            int maior;
+            
+            
+            if (n1 == n2 && n2 == n3)
+            {
+                lblResultado.Text = ("Valores são iguais");
+                return;
+            }
+
+            if (n1 == n2 && n1 > n3 && n2 > n3)
+            {
+                lblResultado.Text = ("Valores " + n1 + " e " + n2 + " são maiores");
+                return;
+            }
+
+            if (n1 == n3 && n1 > n2 && n3 > n2)
+            {
+                lblResultado.Text = ("Valores " + n1 + " e " + n3 + " são maiores");
+                return;
+            }
+
+            if (n2 == n3 && n2 > n1 && n3 > n1)
+            {
+                lblResultado.Text = ("Valores " + n2 + " e " + n3 + " são maiores");
+                return;
+            }
+
+
+            if (n1 > n2 && n1 > n3)
+            {
+                maior = n1;
+            }else if (n2 > n1 && n2 > n3)
+            {
+                maior = n2;
+            }
+            else
+            {
+                maior = n3;
+            }
+
+            lblResultado.Text = "O número " + maior + " é o maior.";
+
+           
+        }
+    }
+}
